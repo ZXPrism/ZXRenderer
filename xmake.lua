75f25291464e19a9fc51f48940b08ab86ce5b768
ZXPrism <zxprism03@gmail.com>
2025-03-16 14:25:56 +0800

    build: update build script to support adding demo targets

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 set_project("ZXRenderer")
 set_version("1.0.0")
+add_defines("ZXRENDEER_VERSION=\"1.0.0\"")
 
 add_rules("mode.debug", "mode.release")
 add_repositories("remote-repo https://github.com/ZXPrism/my-xmake-repo.git")
@@ -20,3 +21,20 @@ target("ZXRenderer")
         os.cp(target:targetfile(), "bin/")
     end)
 target_end()
+
+function AddTarget_Demo(demo_name)
+    target(demo_name)
+        set_languages("cxx23")
+        set_kind("binary")
+        set_warnings("all", "error")
+
+        add_packages("sol2", "libsdl3")
+        add_packages("zxmath")
+        add_files("demo/" .. demo_name .. "/**.cpp")
+        add_includedirs("demo/" .. demo_name .. "/")
+
+        after_build(function (target)
+            os.cp(target:targetfile(), "bin/" .. "demo/" .. demo_name .. "/")
+        end)
+    target_end()
+end
